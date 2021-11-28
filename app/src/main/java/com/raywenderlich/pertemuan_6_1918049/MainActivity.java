799db433e2be98d6a654e620f25d8c6469e07231
amima <ninja_cat712@yahoo.com>
2021-11-28 19:53:06 +0700

    up tugas 6 1918049

--- app/src/main/java/com/raywenderlich/pertemuan_6_1918049/MainActivity.java
+++ app/src/main/java/com/raywenderlich/pertemuan_6_1918049/MainActivity.java
@@ -7,22 +7,25 @@ import androidx.work.WorkManager;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    public TextView status;
+    public ImageView success;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final OneTimeWorkRequest request = new
-                OneTimeWorkRequest.Builder(MyWorker.class).build();
-        findViewById(R.id.button).setOnClickListener(new
-         View.OnClickListener() {
+        final OneTimeWorkRequest request = new OneTimeWorkRequest.Builder(MyWorker.class).build();
+        status = (TextView) findViewById(R.id.textView);
+        success = (ImageView) findViewById(R.id.imageView);
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
              @Override
              public void onClick(View view) {
-                 WorkManager.getInstance().enqueueUniqueWork(
-                         "Notifikasi",
-                         ExistingWorkPolicy.REPLACE,
-                         request);
+                 WorkManager.getInstance().enqueueUniqueWork("Notifikasi",ExistingWorkPolicy.REPLACE,request);
+                 status.setText("status : Succeded");
+                 success.setImageResource(R.drawable.darkmagician);
              }
          });
     }
--- app/src/main/java/com/raywenderlich/pertemuan_6_1918049/MyWorker.java
+++ app/src/main/java/com/raywenderlich/pertemuan_6_1918049/MyWorker.java
@@ -3,6 +3,8 @@ package com.raywenderlich.pertemuan_6_1918049;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.Context;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.core.app.NotificationCompat;
@@ -10,24 +12,22 @@ import androidx.work.Worker;
 import androidx.work.WorkerParameters;
 
 public class MyWorker extends Worker {
+
     public MyWorker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
         super(context, workerParams);
     }
     @NonNull
     @Override
     public Result doWork() {
-        displayNotification("Ini adalah Task Kamu","Task kamusudah selesai");
+        displayNotification("Kerjaan Kamu","Wes Rampung Brooo");
+
         return Result.success();
     }
     public void displayNotification(String task, String desc){
-        NotificationManager manager = (NotificationManager)
-                        getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
-        NotificationChannel channel = new
-                NotificationChannel("terserah",
-                "test123", NotificationManager.IMPORTANCE_HIGH);
+        NotificationManager manager = (NotificationManager)getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
+        NotificationChannel channel = new NotificationChannel("terserah","test123", NotificationManager.IMPORTANCE_HIGH);
         manager.createNotificationChannel(channel);
-        NotificationCompat.Builder builder = new
-                NotificationCompat.Builder(getApplicationContext(),
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext(),
                 "terserah")
                 .setContentTitle(task)
                 .setContentText(desc)
